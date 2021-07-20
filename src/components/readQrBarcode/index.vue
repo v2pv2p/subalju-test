7e5342e1af167840fc8ad8c40dfbfdd9ea6261b8
sujeong_BPNR <sujeong@bpnr.co.kr>
2021-07-20 17:26:33 +0900

    github io에서 사용할수있도록 수정 build

--- src/components/readQrBarcode/index.vue
+++ src/components/readQrBarcode/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    {{ readCode }}
-    <video ref="video" width="350" height="350" autoPlay></video>
+    <div>
+      <video ref="video" width="350" height="350" autoPlay></video>
+    </div>
+    <div>
+      {{ readCode }}
+    </div>
   </div>
 </template>
 
@@ -105,138 +109,6 @@ export default {
   position: relative;
   background-color: #808080;
 
-  .video-area {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    z-index: 0;
-  }
-
-  .cover-area {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    z-index: 1;
-
-    display: grid;
-    grid-template-areas: 'top top top' 'left center right' 'bottom bottom bottom ';
-    grid-template-columns: minmax(0, 1fr) 260px minmax(0, 1fr);
-    grid-template-rows: minmax(0, 1fr) 260px minmax(0, 1fr);
-    height: 100%;
-
-    .center {
-      position: relative;
-      grid-area: center;
-
-      .edge-top-left {
-        position: absolute;
-
-        top: -10px;
-        left: -10px;
-        width: 20px;
-        height: 20px;
-
-        border-top: 6px solid white;
-        border-left: 6px solid white;
-      }
-
-      .edge-top-right {
-        position: absolute;
-
-        top: -10px;
-        right: -10px;
-        width: 20px;
-        height: 20px;
-
-        border-top: 6px solid white;
-        border-right: 6px solid white;
-      }
-
-      .edge-bottom-left {
-        position: absolute;
-
-        bottom: -10px;
-        left: -10px;
-        width: 20px;
-        height: 20px;
-
-        border-bottom: 6px solid white;
-        border-left: 6px solid white;
-      }
-
-      .edge-bottom-right {
-        position: absolute;
-
-        bottom: -10px;
-        right: -10px;
-        width: 20px;
-        height: 20px;
-
-        border-bottom: 6px solid white;
-        border-right: 6px solid white;
-      }
-
-      &::after {
-        content: '';
-        position: absolute;
-        background-color: #ff0000;
-        height: 2px;
-        left: -20px;
-        right: -20px;
-        top: calc(50% - 1px);
-      }
-    }
-
-    .around {
-      background-color: rgba(0, 0, 0, 0.5);
-    }
-
-    .top {
-      grid-area: top;
-      display: flex;
-      flex-direction: column;
-
-      .close-button {
-        flex: 1;
-        display: flex;
-        justify-content: flex-end;
-        align-items: flex-start;
-        padding: 40px 30px 0 0;
-
-        > .v-icon {
-          font-size: 30px;
-          color: #ffffff;
-        }
-      }
-
-      .info-text {
-        padding-bottom: 55px;
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        align-items: flex-end;
-        font-size: 14px;
-        font-weight: normal;
-        color: #ffffff;
-      }
-    }
-
-    .bottom {
-      grid-area: bottom;
-    }
-
-    .left {
-      grid-area: left;
-    }
-
-    .right {
-      grid-area: right;
-    }
-  }
 }
 </style>
 
