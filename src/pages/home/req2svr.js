715940cf4764d3e9a546baaf490813e48b77dc04
sujeong_BPNR <sujeong@bpnr.co.kr>
2021-07-29 12:08:12 +0900

    `github io에서 사용할수있도록 수정 build`

--- src/pages/home/req2svr.js
+++ src/pages/home/req2svr.js
@@ -1,18 +1,19 @@
 import axios from 'axios'
 
 export default {
+  // 페이징처리 추가할것
   getProductInfoOfBarcode( barcodeNum ) {
-    return axios.get( 'http://openapi.foodsafetykorea.go.kr/api/sample/C005/json/1/5' + (barcodeNum ? '/BAR_CD=' + barcodeNum : '') ).then( x => {
+    return axios.get( 'https://openapi.foodsafetykorea.go.kr/api/sample/C005/json/1/5' + (barcodeNum ? '/BAR_CD=' + barcodeNum : '') ).then( x => {
       return x['data']['C005']['row']
     } )
   },
   getProductSaleInfoOfBarcode( barcodeNum ) {
-    return axios.get( 'http://openapi.foodsafetykorea.go.kr/api/sample/I2570/json/1/5' + (barcodeNum ? '/BRCD_NO=' + barcodeNum : '') ).then( x => {
+    return axios.get( 'https://openapi.foodsafetykorea.go.kr/api/sample/I2570/json/1/5' + (barcodeNum ? '/BRCD_NO=' + barcodeNum : '') ).then( x => {
       return x['data']['I2570']['row']
     } )
   },
   getProductNutritionInfo( productName ) {
-    return axios.get( 'http://openapi.foodsafetykorea.go.kr/api/sample/I2790/json/1/5/DESC_KOR=' + productName ).then( x => {
+    return axios.get( 'https://openapi.foodsafetykorea.go.kr/api/sample/I2790/json/1/5/DESC_KOR=' + productName ).then( x => {
       return x['data']['I2790']['row']
     } )
   },
