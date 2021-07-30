dd4aa74dcadb74aec47e9250c2a2b5b8b8ed29fb
sujeong_BPNR <sujeong@bpnr.co.kr>
2021-07-30 15:17:09 +0900

    `github io에서 사용할수있도록 수정 build`

--- src/components/readQrBarcode/index.vue
+++ src/components/readQrBarcode/index.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="read-qr-barcode">
-    <div class="device-select-area">
-      <div class="device-select">
-        <select v-model="selectedDeviceId" @change="getVideoInput">
-          <option v-for="device in devices" :value="device.deviceId">
-            {{ device.label }}
-          </option>
-        </select>
-      </div>
-    </div>
-
     <div class="stream-area">
       <video class="video" ref="video" autoPlay></video>
       <canvas class="canvas" ref="canvas"></canvas>
@@ -35,14 +25,12 @@ export default {
       img: null,
 
       devices: null,
-      selectedDeviceId: null,
 
       readCode: ''
     }
   },
   mounted() {
     this.video = this.$refs['video']
-    this.getVideoInput()
 
   },
   beforeDestroy() {
@@ -62,40 +50,18 @@ export default {
   },
   methods: {
     getVideoInput() {
-      let constraints
-      if( this.selectedDeviceId ) {
-        constraints = { video: { deviceId: { exact: this.selectedDeviceId } } }
-      } else {
-        this.selectedDeviceId = this.video.value
-        constraints = { video: { exact: 'environment' } }
-      }
+      navigator.mediaDevices.getUserMedia( { video: { facingMode: 'environment' } } )
+        .then( stream => {
+          this.stream = stream
+          this.video.srcObject = stream
+          this.video.setAttribute( 'playsinline', true ) // 플레이어 파일이 아닌 스트림 화면으로 보여짐
+          this.video.play() // 실행
 
-      navigator.mediaDevices.getUserMedia( constraints )
-        .then( this.gotStream )
-        .then( ( deviceInfos ) => {
-          this.gotDevices( deviceInfos )
           setTimeout( () => {
-            if( !this.readCode ) {
-              this.quaggarStart()
-            }
+            if( !this.readCode ) this.quaggarStart()
           }, LOOP_INTERVAL )
         } )
         .catch( e => {console.error( 'error : ' + e )} )
-    },
-    gotDevices( deviceInfos ) {
-      this.devices = _.filter( deviceInfos, deviceInfo => deviceInfo.kind === 'videoinput' )
-
-      if( !this.selectedDeviceId ) {
-        this.selectedDeviceId = _.get( this.devices, '0.deviceId' )
-      }
-    },
-    gotStream( stream ) {
-      this.stream = stream
-      this.video.srcObject = stream
-      this.video.setAttribute( 'playsinline', true ) // 플레이어 파일이 아닌 스트림 화면으로 보여짐
-      this.video.play() // 실행
-
-      return navigator.mediaDevices.enumerateDevices()
     },
     quaggarStart() {
       this.canvas = this.$refs['canvas']
@@ -105,30 +71,28 @@ export default {
       this.context.drawImage( this.video, 0, 0, this.canvas.width, this.canvas.height )
       this.img = this.canvas.toDataURL()
 
-      setTimeout( () => {
-        if( !this.readCode ) {
-          this.quaggarStart()
-        }
+      if( this.video.readyState === this.video.HAVE_ENOUGH_DATA ) {
+        Quagga.decodeSingle( {
+          src: this.img,
+          numOfWorkers: 0,  // Needs to be 0 when used within node
+          inputStream: {
+            size: 800  // restrict input-size to be 800px in width (long-side)
+          },
+          decoder: {
+            readers: ['ean_reader'] // List of active readers
+          },
+        }, ( result ) => {
+          if( _.get( result, 'codeResult' ) ) {
+            this.readCode = _.get( result, 'codeResult.code' )
+            this.$emit( 'codeResult', result )
+          } else {
+            console.log( 'not detected' )
+          }
+        } )
+      }
 
-        if( this.video.readyState === this.video.HAVE_ENOUGH_DATA ) {
-          Quagga.decodeSingle( {
-            src: this.img,
-            numOfWorkers: 0,  // Needs to be 0 when used within node
-            inputStream: {
-              size: 800  // restrict input-size to be 800px in width (long-side)
-            },
-            decoder: {
-              readers: ['ean_reader'] // List of active readers
-            },
-          }, ( result ) => {
-            if( _.get( result, 'codeResult' ) ) {
-              this.readCode = _.get( result, 'codeResult.code' )
-              this.$emit( 'codeResult', result )
-            } else {
-              console.log( 'not detected' )
-            }
-          } )
-        }
+      setTimeout( () => {
+        if( !this.readCode ) this.quaggarStart()
       }, LOOP_INTERVAL )
     }
   }
