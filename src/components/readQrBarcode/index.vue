a53cb311360892ce3b7b4931231bb7a208bbd9d2
sujeong_BPNR <sujeong@bpnr.co.kr>
2021-08-10 17:19:55 +0900

    바코드 스캐너 카메라 가져오는 코드 수정

--- src/components/readQrBarcode/index.vue
+++ src/components/readQrBarcode/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="read-qr-barcode">
     <div class="stream-area">
-      <video class="video" ref="video"></video>
+      <video class="video" ref="video" autoPlay></video>
+      <canvas class="canvas" ref="canvas"></canvas>
+      <img class="image" ref='canvasImgFile' :src="img">
     </div>
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
-import { BrowserMultiFormatReader } from '@zxing/library'
+import Quagga from '@ericblade/quagga2'
 
 const LOOP_INTERVAL = 20
 
@@ -16,35 +18,23 @@ export default {
   name: 'readQrBarcode',
   data() {
     return {
-      stream: null,
       video: null,
-      reader: null,
+      stream: null,
+      canvas: null,
+      context: null,
+      img: null,
+
+      selectedDeviceId: null,
+
+      readCode: '',
+      deviceCount: null,
     }
   },
   mounted() {
-    this.video = this.$refs['video']
-
-    navigator.mediaDevices.getUserMedia( { video: { facingMode: 'environment' } } )
-      .then( stream => {
-        this.stream = stream
-
-        this.video.srcObject = stream
-        this.video.setAttribute( 'playsinline', true ) // required to tell iOS safari we don't want fullscreen
-        this.video.play()
-
-        this.reader = new BrowserMultiFormatReader()
-
-        setTimeout( () => this.readLoop(), LOOP_INTERVAL )
-      } )
-      .catch( err => {
-        // this.$alertManager.alert( '알림', '카메라를 사용할 수 없습니다.' ).promise.then( this.closeDialog )
-      } )
+    this.getVideoInput()
   },
   beforeDestroy() {
-    if( this.reader ) {
-      this.reader.reset()
-      this.reader = null
-    }
+    this.readCode = 'readCode is not available'
 
     if( this.video ) {
       this.video.pause()
@@ -59,27 +49,62 @@ export default {
     }
   },
   methods: {
-    readLoop() {
-      if( !this.video ) {
-        return
-      }
+    async getVideoInput() {
+      this.video = this.$refs['video']
+      await navigator.mediaDevices.enumerateDevices().then( ( devices ) => {
+        this.lastDevice = _.chain( devices )
+          .filter( device => device.kind === 'videoinput' )
+          .last()
+          .value()
+        console.log( this.lastDevice )
+      } )
 
-      try {
-        if( this.video.readyState === this.video.HAVE_ENOUGH_DATA ) {
-          const result = this.reader.decode( this.video )
-          if( result ) {
-            this.$emit( 'codeResult', result )
-            return //읽었으면 종료
-          }
-        }
-      } catch( error ) {
-        console.error( 'QR/Barcode reading error', error )
-      }
+      const constraints = { video: { deviceId: this.lastDevice.deviceId ? { exact: this.lastDevice.deviceId } : undefined } }
+      await navigator.mediaDevices.getUserMedia( constraints )
+        .then( stream => {
+          this.stream = stream
+          this.video.srcObject = stream
+          this.video.setAttribute( 'playsinline', true ) // 플레이어 파일이 아닌 스트림 화면으로 보여짐
+          this.video.play() // 실행
 
-      setTimeout( () => this.readLoop(), LOOP_INTERVAL )
+          setTimeout( () => {
+            if( !this.readCode ) {
+              this.quaggarStart()
+            }
+          }, LOOP_INTERVAL )
+        } )
     },
-    closeDialog( params ) {
-      this.$popupManager.close( this, params )
+    quaggarStart() {
+      this.canvas = this.$refs['canvas']
+      this.context = this.canvas.getContext( '2d' )
+      this.canvas.width = this.video.clientWidth
+      this.canvas.height = this.video.clientHeight
+      this.context.drawImage( this.video, 0, 0, this.canvas.width, this.canvas.height )
+      this.img = this.canvas.toDataURL()
+
+      if( this.video.readyState === this.video.HAVE_ENOUGH_DATA ) {
+        Quagga.decodeSingle( {
+          src: this.img,
+          numOfWorkers: 0,  // Needs to be 0 when used within node
+          inputStream: {
+            size: 800  // restrict input-size to be 800px in width (long-side)
+          },
+          decoder: {
+            readers: ['ean_reader'] // List of active readers
+          },
+        }, ( result ) => {
+          if( _.get( result, 'codeResult' ) ) {
+            this.readCode = _.get( result, 'codeResult.code' )
+            this.$emit( 'codeResult', result )
+          } else {
+            console.log( 'not detected' )
+          }
+        } )
+      }
+
+      setTimeout( () => {
+        if( !this.readCode ) this.quaggarStart()
+      }, LOOP_INTERVAL )
     }
   }
 }
